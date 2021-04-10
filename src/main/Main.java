9b23df6c454c59eda834c5a4972855dbfe61a1b7
JohnFonseca <81266870+JohnFonseca@users.noreply.github.com>
2021-04-10 11:23:47 -0300

    Update Main.java
    Essa aqui foi a base do código que foi copiada do vídeo "APNP / JC - Exemplo Programação Paralela". Com ela, poderemos adaptar o trabalho para o que for necessário, no que se refere á programação paralela.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,63 @@ package main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("teste 2");
+	public static void inicializa(float[] vet, int max) {
+		for (int i = 0; i < vet.length; i++)
+			vet[i] = (float) (Math.random() * (float)max)
 	}
-
+	
+	public static void processor(float[] A, float[] B, float[] C, int n) {
+		for (int i = 0; i < n; i++)
+			C[i] = A[i] + B[i];
+	}
+	
+	public static float somaVet(float[] C, int n) {
+		float sum = 0;
+		for (int i = 0; i < n; i++)
+			sum += C[i];
+		
+		return sum;
+	}
+	
+	public static void main(String args[]) {
+		int size = 20;
+		int nThreads = 2;
+		int elemThread = size / nThreads;
+		
+		float [] vetA = new float[size];
+		float [] vetB = new float[size];
+		float [] vetC = new float[size];
+		
+		ThreadProcessor threads[] = new ThreadProcessor[nThreads];
+		
+		inicializa(vetA, 2);
+		inicializa(vetB, 3);
+		
+		processor(vetA, vetB, vetC, size);
+		
+		System.out.println("Versão Sequencial");
+		System.out.println("Soma: " + somaVet(VetC, size));
+		
+		for (int i = 0; i < nThreads; i++) {
+			int start = i * elemThread;
+			int end = start + elemThread;
+			threads[i] = new ThreadProcessor(vetA, vetB, vetC, start, end);
+			threads[i].start();
+		}
+		
+		for (int i = 0; i < nThreads; i++) {
+			try {
+				threads[i].join();
+			}
+			
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		System.out.println("Versão Paralela");
+		System.out.println("Soma: " + somaVet(VetC, size));
+	}
+	
 }
